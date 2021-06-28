16fdf235c8ed3bf0e8f3c968a79d41450327f0d0
joseangel1919 <joseangel327@gmail.com>
2021-06-28 21:55:25 +0200

    segunda subida 28/6

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
+import './Product.css'
 
 export default class Product extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-            brand:"Fictizia s.a"
+            brand:"Fictizia s.a",
              
         }
     }
     
     render() {
 
-        let {name,price,brand} = this.props.details //destructuring
+        let {name,price,brand,url} = this.props.details //destructuring
         console.log(name)
         return (
             <section>
+                <div>
                 <h3>OFERTA</h3>
                 <p>Producto:{name}</p>
                 <p>Precio:{price} </p>
                 <p>Marca: { brand || this.state.brand}</p>
-                
+                <img src={url || this.state.url } alt="" />
+                </div>
+                <button onClick={this.props.remove}>Borrar</button>
             </section>
         )
     }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -15,13 +15,19 @@ constructor(props) {
 
 
 
-    renderProducts = () => this.state.products.map((product,i)=> <Product details={product} key= {i}/>)
+    renderProducts = () => this.state.products.map((product,i)=> <Product details={product} key= {i} remove={()=>this.removeProduct(i)}/>)
     
-    addProduct = () => {
-        let name = prompt("Introduce nombre")
-        let price = prompt("Introduce precio")
+    addProduct = (event) => {
         
-        let product = {name,price};
+
+        event.preventDefault();
+        let name =event.target.elements.name.value
+        let price = event.target.elements.price.value
+        let brand = event.target.elements.brand.value
+        let url  = event.target.elements.url.value
+        let product = {name,price,brand,url}; // Nuevo JSON
+        
+        
 
         this.setState({name})
         this.setState({products: [...this.state.products,product]})
@@ -34,6 +40,13 @@ constructor(props) {
     removeAllProducts = () => this.setState({products:[],name:"--"})
 
 
+    removeProduct = (i) => {
+        let filteredArray = this.state.products.filter((item, pos) => i !== pos);
+        
+        
+        this.setState({products: filteredArray});
+    }
+
 
     
 
@@ -41,7 +54,31 @@ constructor(props) {
     render() {
         return (
             <article>
+
+                <h2>Añade producto</h2>
+                <form onSubmit={this.addProduct}>
+                    <label>Name:</label><br/>
+                    <input type="text" id="name" name="name"/><br/>
+                    <label>Price:</label><br/>
+                    <input type="number" id="price" name="price"/><br/>
+                    <label >Marca:</label><br/>
+                    <input type="text" id="brand" name="brand"/><br/>
+                    <label >URL Imagen:</label><br/>
+                    <input type="url" id="url" name="url"/><br/>
+
+                    <input type="submit" value="Enviar"/>
+                </form>
+
+                <h2>Lista de productos</h2>
+
+
+
+
+
+
+
                 {this.renderProducts()}
+
                 <button onClick= {this.addProduct}>Añadir Producto</button>
                 <button onClick= {this.removeAllProducts}>Borrar todo</button>
                 
