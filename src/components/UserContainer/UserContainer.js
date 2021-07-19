c2e1b8b792b6d18612d6edc6b132cf1fd3959818
joseangel1919 <joseangel327@gmail.com>
2021-07-19 21:58:29 +0200

    ultima actualización

--- src/components/UserContainer/UserContainer.js
+++ src/components/UserContainer/UserContainer.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { constant } from "lodash";
+import React, {useEffect} from "react";
+import{useSelector,useDispatch} from 'react-redux';
+import { fetchUsers } from '../../redux'
+
 
 const UserContainer = () => {
-  return <div>UserContainer</div>;
+  const userData = useSelector(state => state.userR);
+  const dispatch = useDispatch();
+  const [load, setLoad] = useState(false)
+
+  useEffect(() => {
+    if (load===true) dispatch(fetchUsers())
+  }, [load,fetchUsers])
+    
+    return <div>
+      <h1>Lista de Usuarios</h1>
+      {load===false?<button onClick={()=>setLoad(true)}>Descargar</button>:""}
+      {userData.loading?<p>Cargando...</p>
+      :userData.error?(<h2>{userData.error}</h2>)
+      :userData.users.map((user,i)=><p key={i}>{user.name}</p>)
+    
+    }
+
+
+
+    </div>
+   
 };
 
 export default UserContainer;
